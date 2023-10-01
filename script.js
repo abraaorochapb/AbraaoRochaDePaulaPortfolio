14c24096063b359ca1dded30f2fb9c621d696e65
abraaorochapb <abraaorochapb@gmail.com>
2023-10-01 13:00:09 -0300

    Finished the contact section with the needed form, this was the last section of the project

--- script.js
+++ script.js
@@ -56,6 +56,56 @@ education.addEventListener('click', () => {
 portfolio.addEventListener('click', () => {
     main.innerHTML = `<h1 class="portfolioh1">Portfólio</h1>
     <div class="portfoliodiv">
-    <p class="portfoliop1"></p>
+    <p class="portfoliop1">
+    Caso queira ver meus projetos, clique <a href="https://github.com/abraaorochapb" target="_blank">aqui</a> para
+    se direcionar ao meu GitHub, onde pode ver meus projetos, estudos e códigos.
+    <h2 class="portfolioh2">Highlights</h2>
+    <div class="projects">
+    <ul>
+    <li>
+    <a href="https://abraaorochapb.github.io/proj4CursoFrontEndInstagram/" 
+    target="_blank">Clone da página de login do Instagram</a>
+    </li>
+    <li>
+    <a href="https://abraaorochapb.github.io/Notes/" 
+    target="_blank">Bloco de notas</a>
+    </li>
+    <li>
+    <a href="https://abraaorochapb.github.io/GeradorQRCode-JS/" 
+    target="_blank">Gerador de QR Code</a>
+    </li>
+    </ul>
+    <ul>
+    <li>
+    <a href="https://abraaorochapb.github.io/PokedexJS/" 
+    target="_blank">Pokédex</a>
+    </li>
+    <li>
+    <a href="https://abraaorochapb.github.io/AdvancedTodoList-JS/" 
+    target="_blank">Todo List</a>
+    </li>
+    <li>
+    <a href="https://abraaorochapb.github.io/proj3CursoFrontEnd-LandingPage/" 
+    target="_blank">Landing Page</a>
+    </li>
+    </ul>
+    </div>
+    </p>
     </div>`
+})
+
+// aqui estou adicionando um evento de click ao elemento "contact", para mudar o conteúdo da página
+//utilização da tag form para o formulário
+contact.addEventListener('click', () => {
+    main.innerHTML = ` <section class="contact-container">
+    <h2>Contato</h2>
+    <p>Entre em contato comigo</p>
+    <form>
+      <input type="text" name="name" id="name" placeholder="Nome">
+      <input type="email" name="email" id="email" placeholder="E-mail">
+      <textarea name="message" id="message" placeholder="Insira sua mensagem"></textarea>
+      <input type="submit" value="Enviar">
+    </form>
+  </section>
+</main>`
 })
